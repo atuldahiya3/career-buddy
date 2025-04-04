aca8fca65ba69eda80e85d27e85430caf2e12cab
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-04-05 01:21:24 +0530

    hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/HeroSection";
 import { Accordion } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -5,10 +6,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      helloo
-      <Button>new button</Button>
-      <br/>
-      <Accordion>hello</Accordion>
+      <div className="grid-background">
+        <HeroSection/>
+      </div>
     </div>
   );
 }
---
+++ components/HeroSection.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full pt-36 flex justify-center items-center">
+      <div className="text-center">
+        <div className="p-20">
+          <h1 className="text-7xl font-bold bg-gradient-to-b from-gray-500 to-gray-300 bg-clip-text text-transparent">
+            Your AI Career Buddy
+          </h1>
+          <h1 className="text-5xl font-bold bg-gradient-to-b from-gray-500 to-gray-100 bg-clip-text text-transparent">
+            for Professional Success
+          </h1>
+          <p className="text-xl bg-gradient-to-r from-gray-200 to-gray-50 bg-clip-text text-transparent mt-10">
+            Get the best resources, tools and mentorship to advance your career
+            with personalized AI-driven insights and resources.
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-8 px-20">
+          <Link href="/dashboard">
+            <Button className="size-lg font-bold text-xl px-8">Get Started</Button>
+          </Link>
+          <Link href="/https://www.youtube.com/watch?v=O7tvHh3MPSs&list=RDO7tvHh3MPSs&start_radio=1">
+            <Button className="size-lg font-bold text-xl px-8" variant="outline">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="p-20 w-full">
+          <div>
+            <Image
+              src={"/hero.jpg"}
+              height={1080}
+              width={720}
+              alt="hero image"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
